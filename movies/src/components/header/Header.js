377760a05eecf080670886973f70fa876465c7b8
olygood <olivier.huttmacher@gmail.com>
2020-04-19 18:33:12 +0200

    mise a jour du header et public/index.html

--- movies/src/components/header/Header.js
+++ movies/src/components/header/Header.js
@@ -3,6 +3,23 @@ import React, {Component} from 'react'
 
 export default class Header extends Component{
     render(){
-        return <header>Header</header>
+        return(
+            <header className="navbar navbar-expand-lg navbar-dark bg-primary">
+                <a className="navbar-brand"href="/">AlloMovies</a>
+                    <button className="navbar-toggler">
+                        <span className="navbar-toglgler-icon"></span>
+                            </button>
+                                <div className="collapse navbar-collapse">
+                                    <ul className="navbar-nav ml-auto">
+                                        <li className="nav-item active">
+                                            <a className="nav-link"href="/">HOME</a>
+                                                 </li>
+                                                    <li className="nav-item">
+                                                        <a className="nav-link"href="/">Favoris</a>
+                                                            </li>
+                                                                </ul>
+                                                                    </div>
+                                                                        </header>
+        )
     }
 }
